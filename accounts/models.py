421856ac942debf115e6418bd02600ad952df52a
huangqimin <huangqimin@lvye.cn>
2014-06-19 05:35:48 +0000

    add link for user's info

--- accounts/models.py
+++ accounts/models.py
@@ -6,12 +6,12 @@ from django.utils.translation import ugettext_lazy as _, ugettext
 
 # 用户注册信息表
 class UserInfo(models.Model):
-    username = models.CharField(_(u'username'), max_length=255, blank=True, null=True, help_text='用户名')
-    password = models.CharField(_(u'password'), max_length=255, blank=True, null=True, help_text='密码')
-    email = models.EmailField(_(u'email'), max_length=255, blank=True, null=True, help_text='邮箱')
-    login_page = models.URLField(_(u'login_page'), max_length=255, blank=True, null=True, help_text='登录首页')
-    display_bg = models.BooleanField(_('display_bg'), default=True, help_text='是否显示背景图')
-    company = models.CharField(_(u'company'), max_length=255, blank=True, null=True, help_text='所在公司')
+    username = models.CharField(_(u'username'), max_length=255, blank=True, null=True, help_text=u'用户名')
+    password = models.CharField(_(u'password'), max_length=255, blank=True, null=True, help_text=u'密码')
+    email = models.EmailField(_(u'email'), max_length=255, blank=True, null=True, help_text=u'邮箱')
+    login_page = models.URLField(_(u'login_page'), max_length=255, blank=True, null=True, help_text=u'登录首页')
+    display_bg = models.BooleanField(_('display_bg'), default=True, help_text=u'是否显示背景图')
+    company = models.CharField(_(u'company'), max_length=255, blank=True, null=True, help_text=u'所在公司')
     github = models.CharField(_(u'github'), max_length=255, blank=True, null=True, help_text='Github')
     sof = models.CharField(_(u'stackoverflow'), max_length=255, blank=True, null=True, help_text='StackOverflow')
     weibo = models.CharField(_(u'weibo'), max_length=255, blank=True, null=True, help_text='Weibo')
@@ -19,9 +19,9 @@ class UserInfo(models.Model):
     v2ex = models.CharField(_(u'v2ex'), max_length=255, blank=True, null=True, help_text='v2ex')
     blog = models.CharField(_(u'blog'), max_length=255, blank=True, null=True, help_text='Blog')
     btc = models.CharField(_(u'btc'), max_length=255, blank=True, null=True, help_text='btc address')
-    freeze = models.BooleanField(_('freeze'), default=False, help_text='是否被冻结 True for freeze && False for not freeze')
-    create_time = models.DateTimeField(_(u'createtime'), auto_now_add=True, editable=True, help_text='注册时间')
-    modify_time = models.DateTimeField(_(u'modifytime'), auto_now=True, editable=True, help_text='修改时间')
+    freeze = models.BooleanField(_('freeze'), default=False, help_text=u'是否被冻结 True for freeze && False for not freeze')
+    create_time = models.DateTimeField(_(u'createtime'), auto_now_add=True, editable=True, help_text=u'注册时间')
+    modify_time = models.DateTimeField(_(u'modifytime'), auto_now=True, editable=True, help_text=u'修改时间')
 
     class Meta:
         db_table = u'userinfo'
