b227016d386bf454f736031b12e92442249a89e4
kimi.huang <kimi.huang@brightcells.com>
2015-05-26 13:44:09 +0800

    change home page & change to use cdn

--- dh/urls.py
+++ dh/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls import patterns, include, url
 
 
 urlpatterns = patterns('dh.views',
-    url(r'^$', 'dh_home', name='dh_home'),
-    url(r'^index/$', 'dh', name='dh'),
-    url(r'^home/$', 'dh', name='dh'),
+    url(r'^$', 'dh3', name='dh_home'),
+    url(r'^index/$', 'dh3', name='dh'),
+    url(r'^home/$', 'dh3', name='dh'),
 
     url(r'^home2/$', 'dh2', name='dh2'),
     url(r'^home3/$', 'dh3', name='dh3'),
